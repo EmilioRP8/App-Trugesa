a625a4b254c836d283c9b9108ae5243b6396819c
EmilioRP8 <a01737819@tec.mx>
2025-06-03 17:04:28 -0600

    Add files via upload

---
+++ CotizacionesT/cot.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="Cotizaciones Trugesa", page_icon="📉")
+
+st.image("tr.png", use_container_width=True)
+st.markdown("## **TRUGESA TRANSPORTACIÓN ESPECIALIZADA**")
+
+Carga = st.Page("pag/carga.py", title="Carga", icon="🚛")
+Pasaje = st.Page("pag/pasaje.py", title="Pasaje", icon="🚌")
+Turismo = st.Page("pag/turismo.py", title="Turismo", icon="🧳")
+Ejecutivo = st.Page("pag/ejecutivo.py", title="Ejecutivo", icon="👔")
+
+pg = st.navigation({
+    "Tipos de Servicio": [Carga, Pasaje, Turismo, Ejecutivo]
+})
+
+pg.run()
